ddc33d2d898c5a135ed7637871fddd55b701f53e
JACK <109014373+JACKSAMAA@users.noreply.github.com>
2022-08-29 15:16:55 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -780,7 +780,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendphoto(
+            dispatcher.bot.sendmedia(
                 f"@{SUPPORT_CHAT}",
                 photo="https://te.legra.ph/file/bb8dd7a793ad9cf1d5a55.jpg",
                 caption=f"""
