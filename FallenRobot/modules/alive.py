f5a2edcdc02766a1e2582dc7800bef96a07d8da8
JACK <109014373+JACKSAMAA@users.noreply.github.com>
2022-08-31 12:32:17 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -18,9 +18,9 @@ PHOTO = [
 
 @register(pattern=("Kazuha alive"))
 async def awake(event):
-    TEXT = f"**Bʀᴜʜʜ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nI Aᴍ {dispatcher.bot.first_name}** \n\n"
+    TEXT = f"**Bʀᴜʜʜ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n❆ I Aᴍ {dispatcher.bot.first_name}** \n\n"
     TEXT += f"❆ **I'ʟʟ Bᴇ Gɪᴠɪɴɢ Mʏ Bᴇsᴛ Iɴ Gᴀᴍᴇ Fᴏʀ Yᴏᴜ Bʀᴜʜʜ.** \n\n"
-    TEXT += f"❆ **Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ​ : [Jᴇᴀɴ](https://t.me/{OWNER_USERNAME})** \n\n"
+    TEXT += f"❆ **Mʏ Bʀᴏᴛʜᴇʀ : [Jᴇᴀɴ](https://t.me/{OWNER_USERNAME})** \n\n"
     TEXT += f"❆ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"❆ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"❆ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
