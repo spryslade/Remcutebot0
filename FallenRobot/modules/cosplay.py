1ba546c53a8df06897e5775ee8d3741698ec0f7d
JEAN SAMA <109014373+JACKSAMAA@users.noreply.github.com>
2022-09-02 18:22:42 +0530

    Update cosplay.py

--- FallenRobot/modules/cosplay.py
+++ FallenRobot/modules/cosplay.py
@@ -22,7 +22,7 @@ PHOTO = [
 async def awake(event):
     TEXT = "Cosplay"   
     ran = random.choice(PHOTO)
-    await tbot.send_pic(ran, caption=TEXT)
+    await tbot.send_file(event.chat_id, ran, caption=TEXT)
 
 
 __mod_name__ = "Cosplay"
