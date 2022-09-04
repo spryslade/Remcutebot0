1c79ae4b0764149a0b1f38c9f721491cacc27966
JEAN SAMA <109014373+JACKSAMAA@users.noreply.github.com>
2022-09-04 10:28:23 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -11,20 +11,20 @@ from FallenRobot import telethn as tbot, SUPPORT_CHAT, OWNER_USERNAME, dispatche
 
 
 PHOTO = [
-    "https://telegra.ph/file/354c77ca7f1f7b03d87e7.jpg",
-    "https://te.legra.ph/file/2187067613c1902b4c3ad.jpg", 
+    "https://telegra.ph/file/47487d8437bd316d98feb.jpg",
+    "https://telegra.ph/file/3bfc172ba4b2eaf3892dc.jpg", 
 ]
 
 
 @register(pattern=("Kazuha alive"))
 async def awake(event):
-    TEXT = f"**Bʀᴜʜʜ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n❆ I Aᴍ {dispatcher.bot.first_name}** \n\n"
-    TEXT += f"❆ **I'ʟʟ Bᴇ Gɪᴠɪɴɢ Mʏ Bᴇsᴛ Iɴ Gᴀᴍᴇ Fᴏʀ Yᴏᴜ Bʀᴜʜʜ.** \n\n"
-    TEXT += f"❆ **Mʏ Oᴡɴᴇʀ : [Jᴇᴀɴ](https://t.me/{OWNER_USERNAME})** \n\n"
+    TEXT = f"[{event.sender.first_name}](tg://user?id={event.sender.id}) Kᴜɴɴ, \n\n❆ I Aᴍ {dispatcher.bot.first_name}** \n\n"
+    TEXT += f"❆ **I'ʟʟ Bᴇ Gɪᴠɪɴɢ Mʏ Bᴇsᴛ.** \n\n"
+    TEXT += f"❆ **Mʏ Oᴡɴᴇʀ :** [Jᴇᴀɴ](https://t.me/{OWNER_USERNAME})** \n\n"
     TEXT += f"❆ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"❆ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"❆ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
-    TEXT += f"❆ Tʜᴀɴᴋs Fᴏʀ Aᴅᴅɪɴɢ Mᴇ Hᴇʀᴇ Bʀᴜʜʜ. \n\n"
+    TEXT += f"❆ **Tʜᴀɴᴋs Fᴏʀ Aᴅᴅɪɴɢ Mᴇ Hᴇʀᴇ.** \n\n"
     BUTTON = [
         [
             Button.url("Hᴇʟᴘ​", f"https://t.me/{dispatcher.bot.username}?start=help"),
